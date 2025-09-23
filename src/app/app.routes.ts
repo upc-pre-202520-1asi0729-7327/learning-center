1762dbaf31d38b8b9435dbdf2301098353d03b05
Angel Velasquez <pcisavel@upc.edu.pe>
2025-09-22 20:51:58 -0500

    feat(routes): update routing configuration with home, about, and 404 page.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import {Home} from './shared/presentation/views/home/home';
+const about = () => import('./shared/presentation/views/about/about').then(m => m.About);
+const pageNotFound = () => import('./shared/presentation/views/page-not-found/page-not-found').then(m => m.PageNotFound);
 
-export const routes: Routes = [];
+const baseTitle = 'ACME Learning Center';
+export const routes: Routes = [
+  { path: 'home',   component: Home,              title: `Home - ${baseTitle}` },
+  { path: 'about',  loadComponent: about,         title: `About - ${baseTitle}` },
+  { path: '',       redirectTo: '/home',          pathMatch: 'full' },
+  { path: '**',     loadComponent: pageNotFound,  title:  `Page Not Found - ${baseTitle}` }
+];
