b3f83f0a76f46c0d1287a989c32781e15f03a3c8
Angel Velasquez <pcisavel@upc.edu.pe>
2025-09-22 21:01:24 -0500

    feat(layout): integrate layout component and add translation support.

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,21 @@
-import { Component, signal } from '@angular/core';
+import {Component, inject, signal} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {Layout} from './shared/presentation/components/layout/layout';
+import {TranslateService} from '@ngx-translate/core';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [Layout],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('learning-center');
+  private translate: TranslateService;
+
+  constructor() {
+    this.translate = inject(TranslateService);
+    this.translate.addLangs(['en', 'es']);
+    this.translate.use('en');
+  }
 }
