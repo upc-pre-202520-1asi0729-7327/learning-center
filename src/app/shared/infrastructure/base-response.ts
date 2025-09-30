93cb3c28b766a1d05a0ec36ef81b2508783b3f85
Angel Velasquez <pcisavel@upc.edu.pe>
2025-09-29 19:48:07 -0500

    feat(api): add abstract base response and resource interfaces.

---
+++ src/app/shared/infrastructure/base-response.ts
@@ -0,0 +1,12 @@
+/**
+ * Abstract base response interface for API responses.
+ */
+export interface BaseResponse {
+}
+
+/**
+ * Abstract base resource interface for API resources.
+ */
+export interface BaseResource {
+  id: number;
+}
