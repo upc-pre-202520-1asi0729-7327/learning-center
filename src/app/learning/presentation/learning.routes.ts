b6552edad9dfb426f681dd4f9dc4a08544185e58
Angel Velasquez <pcisavel@upc.edu.pe>
2025-09-29 21:37:56 -0500

    feat(learning): add routing for categories and courses with lazy-loaded components.

---
+++ src/app/learning/presentation/learning.routes.ts
@@ -0,0 +1,17 @@
+import { Routes } from '@angular/router';
+// Lazy-loaded components
+const categoryList = () => import('./views/category-list/category-list').then(m => m.CategoryList);
+const categoryForm = () => import('./views/category-form/category-form').then(m => m.CategoryForm);
+const courseList = () => import('./views/course-list/course-list').then(m => m.CourseList);
+const courseForm = () => import('./views/course-form/course-form').then(m => m.CourseForm);
+
+const learningRoutes: Routes = [
+  { path: 'categories',           loadComponent: categoryList},
+  { path: 'categories/new',       loadComponent: categoryForm},
+  { path: 'categories/:id/edit',  loadComponent: categoryForm},
+  { path: 'courses',              loadComponent: courseList},
+  { path: 'courses/new',          loadComponent: courseForm},
+  { path: 'courses/:id/edit',     loadComponent: courseForm}
+];
+
+export default learningRoutes;
