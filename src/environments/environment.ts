ae9b0400dea6b9e00f83db0a916681f0211b851f
Angel Velasquez <pcisavel@upc.edu.pe>
2025-09-29 20:35:32 -0500

    feat(environment): add development and production environment configurations.

---
+++ src/environments/environment.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: true,
+  platformProviderApiBaseUrl: 'http://localhost:3000/api/v1',
+  platformProviderCategoriesEndpointPath: '/categories',
+  platformProviderCoursesEndpointPath: '/courses'
+};
