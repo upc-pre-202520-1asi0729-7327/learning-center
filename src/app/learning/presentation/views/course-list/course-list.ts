b7f4279d1d15fb7ecc0dd9d7dea73a1c984a354c
Angel Velasquez <pcisavel@upc.edu.pe>
2025-10-01 15:57:13 -0500

    feat(courses): enhance course list with material table, loading spinner, error handling, and actions.

--- src/app/learning/presentation/views/course-list/course-list.ts
+++ src/app/learning/presentation/views/course-list/course-list.ts
@@ -1,11 +1,51 @@
-import { Component } from '@angular/core';
+import {Component, inject} from '@angular/core';
+import {LearningStore} from '../../../application/learning.store';
+import {Router} from '@angular/router';
+import {MatProgressSpinner} from '@angular/material/progress-spinner';
+import {MatError} from '@angular/material/form-field';
+import {
+  MatCell,
+  MatCellDef,
+  MatColumnDef,
+  MatHeaderCell,
+  MatHeaderCellDef,
+  MatHeaderRow, MatHeaderRowDef, MatRow, MatRowDef,
+  MatTable
+} from '@angular/material/table';
+import {MatButton} from '@angular/material/button';
 
 @Component({
   selector: 'app-course-list',
-  imports: [],
+  imports: [
+    MatProgressSpinner,
+    MatError,
+    MatTable,
+    MatColumnDef,
+    MatHeaderCell,
+    MatCell,
+    MatCellDef,
+    MatHeaderCellDef,
+    MatButton,
+    MatHeaderRow,
+    MatRow,
+    MatHeaderRowDef,
+    MatRowDef
+  ],
   templateUrl: './course-list.html',
   styleUrl: './course-list.css'
 })
 export class CourseList {
+  readonly store = inject(LearningStore);
+  protected router = inject(Router);
+
+  displayedColumns: string[] = ['id', 'title', 'description', 'category', 'actions'];
+
+  editCourse(id: number) {
+    this.router.navigate(['learning/courses', id, 'edit']).then();
+  }
+
+  deleteCourse(id: number) {
+    this.store.deleteCourse(id);
+  }
 
 }
