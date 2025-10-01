966a11a11dd5a825d2fdf563c282708e2c6fdd34
Angel Velasquez <pcisavel@upc.edu.pe>
2025-10-01 15:56:53 -0500

    feat(learning): enhance category list with material table, loading spinner, and error handling.

--- src/app/learning/presentation/views/category-list/category-list.ts
+++ src/app/learning/presentation/views/category-list/category-list.ts
@@ -1,11 +1,50 @@
-import { Component } from '@angular/core';
+import {Component, inject} from '@angular/core';
+import {LearningStore} from '../../../application/learning.store';
+import {Router} from '@angular/router';
+import {MatError} from '@angular/material/form-field';
+import {MatProgressSpinner} from '@angular/material/progress-spinner';
+import {
+  MatCell,
+  MatCellDef,
+  MatColumnDef,
+  MatHeaderCell,
+  MatHeaderCellDef,
+  MatHeaderRow, MatHeaderRowDef, MatRow, MatRowDef,
+  MatTable
+} from '@angular/material/table';
+import {MatButton} from '@angular/material/button';
 
 @Component({
   selector: 'app-category-list',
-  imports: [],
+  imports: [
+    MatError,
+    MatProgressSpinner,
+    MatTable,
+    MatColumnDef,
+    MatHeaderCell,
+    MatCell,
+    MatHeaderCellDef,
+    MatCellDef,
+    MatButton,
+    MatHeaderRow,
+    MatHeaderRowDef,
+    MatRowDef,
+    MatRow
+  ],
   templateUrl: './category-list.html',
   styleUrl: './category-list.css'
 })
 export class CategoryList {
+  readonly store = inject(LearningStore);
+  protected router = inject(Router);
 
+  displayedColumns: string[] = ['id', 'name', 'actions'];
+
+  editCategory(id: number) {
+    this.router.navigate(['learning/categories',id,'edit']).then();
+  }
+
+  deleteCategory(id: number) {
+    this.store.deleteCategory(id);
+  }
 }
