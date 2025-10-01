72d9348ff36f3cc026aab006ae4d4a6a9b46b6b3
Angel Velasquez <pcisavel@upc.edu.pe>
2025-10-01 15:56:16 -0500

    refactor(routes): rename lazy-loaded learning routes import for clarity.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,13 +2,13 @@ import { Routes } from '@angular/router';
 import {Home} from './shared/presentation/views/home/home';
 const about = () => import('./shared/presentation/views/about/about').then(m => m.About);
 const pageNotFound = () => import('./shared/presentation/views/page-not-found/page-not-found').then(m => m.PageNotFound);
-const learningRoutesChildren = () => import('./learning/presentation/learning.routes').then(m => m.default);
+const learningRoutes = () => import('./learning/presentation/learning.routes').then(m => m.learningRoutes);
 
 const baseTitle = 'ACME Learning Center';
 export const routes: Routes = [
   { path: 'home',     component:      Home,                     title: `Home - ${baseTitle}` },
   { path: 'about',    loadComponent:  about,                    title: `About - ${baseTitle}` },
-  { path: 'learning', loadChildren:   learningRoutesChildren },
+  { path: 'learning', loadChildren:   learningRoutes },
   { path: '',         redirectTo:     '/home',                  pathMatch: 'full' },
   { path: '**',       loadComponent:  pageNotFound,             title:  `Page Not Found - ${baseTitle}` }
 ];
