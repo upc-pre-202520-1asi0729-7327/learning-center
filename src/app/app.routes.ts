e426e62053648ef08fbea3a6de15445adf0b2b4a
Angel Velasquez <pcisavel@upc.edu.pe>
2025-11-19 17:50:57 -0500

    feat(routes): add iam routes for user authentication and management.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,7 @@ import {Home} from './shared/presentation/views/home/home';
 const about = () => import('./shared/presentation/views/about/about').then(m => m.About);
 const pageNotFound = () => import('./shared/presentation/views/page-not-found/page-not-found').then(m => m.PageNotFound);
 const learningRoutes = () => import('./learning/presentation/learning.routes').then(m => m.learningRoutes);
+const iamRoutes = () => import('./iam/presentation/iam.routes').then(m => m.iamRoutes);
 
 const baseTitle = 'ACME Learning Center';
 
@@ -15,6 +16,7 @@ export const routes: Routes = [
   { path: 'home',     component:      Home,                     title: `Home - ${baseTitle}` },
   { path: 'about',    loadComponent:  about,                    title: `About - ${baseTitle}` },
   { path: 'learning', loadChildren:   learningRoutes },
+  { path: 'iam',      loadChildren:   iamRoutes },
   { path: '',         redirectTo:     '/home',                  pathMatch: 'full' },
   { path: '**',       loadComponent:  pageNotFound,             title:  `Page Not Found - ${baseTitle}` }
 ];
